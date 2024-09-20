783fab09c0619e25bd7e643adc5733300d6a3a94
henry <henry@everylink.ai>
2024-09-20 18:03:04 +0800

    delete search.

--- src/components/token-data-table.tsx
+++ src/components/token-data-table.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import * as React from 'react'
-import { ChevronDown, ChevronUp, ChevronsUpDown, Search } from 'lucide-react'
+import { ChevronDown, ChevronUp, ChevronsUpDown } from 'lucide-react'
 import {
 	ColumnDef,
 	ColumnFiltersState,
